ea01a979c232a7b6d245b49681007862f3d8dff5
Benhur <benhurbbs@gmail.com>
2022-07-04 09:05:42 -0300

    Início do curso de Java Heranças

--- bytebank-encapsulado/src/Conta.java
+++ bytebank-encapsulado/src/Conta.java
@@ -4,7 +4,7 @@ public class Conta {
 	private int agencia;
 	private int numero;
 	private Cliente titular;
-	private static int total;
+	private static int total = 0;
 	
 	
 	public Conta(int agencia, int numero) {
---
+++ bytebank-herdado/src/TesteGerente.java
@@ -0,0 +1,17 @@
+
+public class TesteGerente {
+	public static void main(String[] args) {
+		Gerente g1 = new Gerente();
+		g1.setNome("Benhur");
+		g1.setCpf("12345678925");
+		g1.setSalario(5000.0);
+		
+		System.out.println(g1.getNome());
+		System.out.println(g1.getCpf());
+		System.out.println(g1.getSalario());
+		
+		g1.setSenha(222);
+		boolean autenticou = g1.autentica(222);
+		System.out.println(autenticou);
+	}
+}
