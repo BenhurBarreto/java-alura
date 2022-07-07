8ac38d81aa46bed79d69f91297da8d5d6d2c7793
Benhur <benhurbbs@gmail.com>
2022-07-07 09:17:21 -0300

    Correção método transfere e aprendendo mais sobre herança e polimorfismo

--- bytebank-herdado-conta/src/ContaCorrente.java
+++ bytebank-herdado-conta/src/ContaCorrente.java
@@ -5,4 +5,10 @@ public class ContaCorrente extends Conta {
 	public ContaCorrente(int agencia, int numero) {
 		super(agencia, numero);// chama o construtor da classe mãe
 	}
+	
+	@Override
+	public boolean saca(double valor) {
+		double valorASacar = valor + 0.2;
+		return super.saca(valorASacar);
+	}
 }
