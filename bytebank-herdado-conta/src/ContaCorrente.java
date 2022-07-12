08f6b79c220a91a8c6eda8e0b717857958bd3d88
Benhur <benhurbbs@gmail.com>
2022-07-12 09:43:09 -0300

    Finalizando o curso de polimorfismo com mais conhecimentos de tipos de heranças

--- bytebank-herdado-conta/src/ContaCorrente.java
+++ bytebank-herdado-conta/src/ContaCorrente.java
@@ -1,5 +1,5 @@
 
-public class ContaCorrente extends Conta {
+public class ContaCorrente extends Conta implements Tributavel {
 	
 	
 	public ContaCorrente(int agencia, int numero) {
@@ -15,5 +15,10 @@ public class ContaCorrente extends Conta {
 	@Override
 	public void deposita(double valor) {
 		super.saldo += valor;
+	}
+
+	@Override
+	public double getValorImposto() {
+		return super.saldo * 0.01;
 	}
 }
