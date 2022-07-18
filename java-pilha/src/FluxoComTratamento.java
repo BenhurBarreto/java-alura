1826e514c5f30cd9bd76c2275de7a3e7c2bd9102
Benhur <benhurbbs@gmail.com>
2022-07-18 09:13:24 -0300

    Conhecendo sobre Exceções, hierarquia, checked e unchecked

--- java-pilha/src/FluxoComTratamento.java
+++ java-pilha/src/FluxoComTratamento.java
@@ -1,11 +1,11 @@
 
-public class Fluxo {
+public class FluxoComTratamento {
 	
 	public static void main(String[] args) {
         System.out.println("Ini do main");
         try {
         	metodo1();
-        } catch (ArithmeticException | NullPointerException | MinhaExcecao ex) {
+        } catch (ArithmeticException | NullPointerException ex) {
         	String msg = ex.getMessage();
         	System.out.println("Exception " + msg);
         	ex.printStackTrace();
@@ -13,19 +13,21 @@ public class Fluxo {
         System.out.println("Fim do main");
     }
 
-    private static void metodo1() throws MinhaExcecao{
+    private static void metodo1() {
         System.out.println("Ini do metodo1");
         metodo2();        
         System.out.println("Fim do metodo1");
     }
 
-    private static void metodo2() throws MinhaExcecao{
+    private static void metodo2() {
         System.out.println("Ini do metodo2");
-        
-        throw new MinhaExcecao("Deu errado");
-//        throw new ArithmeticException("Deu errado");
-        
-//        System.out.println("Fim do metodo2");
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(i);            
+            int a = i / 0;
+//            Conta c = null;
+//            c.deposita();
+        }
+        System.out.println("Fim do metodo2");
     }
 	
 }
