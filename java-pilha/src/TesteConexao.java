8007f9fb5ad91869baeef393565dd8051c431c56
Benhur <benhurbbs@gmail.com>
2022-07-21 08:47:27 -0300

    Try with resources

---
+++ java-pilha/src/TesteConexao.java
@@ -0,0 +1,30 @@
+
+public class TesteConexao {
+
+	public static void main(String[] args) {
+		
+		try (Conexao conexao = new Conexao()) {
+			conexao.leDados();
+		} catch (IllegalStateException ex) {
+			System.out.println("Deu erro na conexao");
+		}
+		
+		// ---------------
+		
+//		Conexao con = null;
+//		try {
+//			con = new Conexao();
+//			con.leDados();			
+//		} catch (IllegalStateException ex) {
+//			System.out.println("Deu erro na conexao");			
+//		} finally {
+//			System.out.println("Finally");
+//			if (con != null) {
+//				con.close();
+//			}
+//			
+//		}
+
+	}
+
+}
