bfe40a30059221ae755c3e4e89347c6790528232
Benhur <benhurbbs@gmail.com>
2022-07-26 09:22:31 -0300

    A classe Object e como reescrevê-la

--- bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -90,4 +90,9 @@ public abstract class Conta {
 	public static int getTotal() {
 		return Conta.total;
 	}
+	
+	@Override
+	public String toString() {
+		return "Numero: " + this.numero + ", Agencia: " + this.agencia;
+	}
 }
