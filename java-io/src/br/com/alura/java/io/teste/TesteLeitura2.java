9efc09f6415bd11e1ab48b168d601900e424a68c
Benhur <benhurbbs@gmail.com>
2022-08-09 19:46:35 -0300

    Java.util.Scanner

---
+++ java-io/src/br/com/alura/java/io/teste/TesteLeitura2.java
@@ -0,0 +1,42 @@
+package br.com.alura.java.io.teste;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TesteLeitura2 {
+
+	public static void main(String[] args) throws Exception {
+		
+		Scanner scanner = new Scanner(new File("contas.csv"));
+		
+		
+		while(scanner.hasNextLine()) {
+			String linha = scanner.nextLine();
+//			System.out.println(linha);
+			
+			Scanner linhaScanner = new Scanner(linha);
+			linhaScanner.useLocale(Locale.US);
+			linhaScanner.useDelimiter(",");
+			
+			String tipoConta = linhaScanner.next();
+			int agencia = linhaScanner.nextInt();
+			int numero = linhaScanner.nextInt();
+			String titular = linhaScanner.next();
+			Double saldo = linhaScanner.nextDouble();
+			
+			String valorFormatado = String.format(new Locale("pt", "BR"), "%s - %04d-%08d, %20s: %010.2f", 
+					tipoConta, agencia, numero, titular, saldo);
+			System.out.println(valorFormatado);
+			
+			linhaScanner.close();
+			
+//			String[] valores = linha.split(",");
+//			System.out.println(valores[3]);
+		}
+		
+		scanner.close();
+	}
+
+}
