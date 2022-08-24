9a2c12423c9dfd139fb2b277ad97fe0993427728
Benhur <benhurbbs@gmail.com>
2022-08-24 08:25:44 -0300

    Outros sets e iterators em Java Collections

--- gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
+++ gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
@@ -1,5 +1,8 @@
 package br.com.alura;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class TestaCursoComAluno {
 
 	public static void main(String[] args) {
@@ -19,9 +22,17 @@ public class TestaCursoComAluno {
 		javaColecoes.matricula(a3);
 		
 		System.out.println("Todos os alunos matriculados: ");
-		javaColecoes.getAlunos().forEach(a -> {
-			System.out.println(a);
-		});
+		
+		Set<Aluno> alunos = javaColecoes.getAlunos();
+		Iterator<Aluno> iterator = alunos.iterator();
+		while(iterator.hasNext()) {
+			Aluno proximo = iterator.next();
+			System.out.println(proximo);
+		}
+		
+//		javaColecoes.getAlunos().forEach(a -> {
+//			System.out.println(a);
+//		});
 		
 		System.out.println("O aluno " + a1 + "está matriculado?");
 		System.out.println(javaColecoes.estaMatriculado(a1));
