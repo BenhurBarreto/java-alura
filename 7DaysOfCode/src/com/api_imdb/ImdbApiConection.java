da44dc58e1d7c85865a657125cd4492e2facb260
Benhur <benhurbbs@gmail.com>
2022-09-01 08:28:09 -0300

    Primeira tarefa do 7daysOfCode: imprimir o JSON no console

--- 7DaysOfCode/src/com/api_imdb/ImdbApiConection.java
+++ 7DaysOfCode/src/com/api_imdb/ImdbApiConection.java
@@ -1,10 +1,25 @@
 package com.api_imdb;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ImdbApiConection {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest
+	            .newBuilder()
+	            .uri(new URI("https://imdb-api.com/en/API/Top250Movies/k_jfp8naw7"))
+	            .GET()
+	            .build();
+		System.out.println(request);
+		
+		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		System.out.println(response.body());
 	}
 
 }
